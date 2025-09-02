22955fb823f5fd308d9146a833ee67289c9231a2
zewied <moaazzakizewied@gmail.com>
2025-09-02 08:59:41 +0300

    assigment

--- Assignment/problem04.cpp
+++ Assignment/problem04.cpp
@@ -12,6 +12,17 @@ int main()
     cout << (num / num) * (num + num) << "\n"; // 6
     cout << (num % num) + (num + num) << "\n"; // 6
 
-    cout << 10 + 20 + 15 + 3 -  190 + 10 +  400 << "\n"; // 0
-} 
+    cout << (-10 + 20) - (15 / 3) * (400 / (190 + 10)) << "\n"; // 0
 
+    /*
+      Write Code Here
+      Do Not Use num = 6 :)
+      */
+    num += num;
+
+    // Do Not Edit
+    cout << num << "\n"; // 6
+
+    cout << ((int)10.5 * 5) + (int)(40.5 + 10.1) << "\n"; // 100
+    
+}
